3eb6c89add26fc8e53c1bf9ffdd46603777ae10c
Danielle Meyerson <danimeye@umich.edu>
2017-02-16 16:23:18 -0500

    uploading first version of problem set 5

---
+++ 206W17_HW5.py
@@ -0,0 +1,110 @@
+import unittest
+import tweepy
+import requests
+import json
+import twitter_info
+
+## SI 206 - W17 - HW5
+## COMMENT WITH:
+## Your section day/time: Friday 1-2pm 
+## Any names of people you worked with on this assignment: n/a 
+
+######## 500 points total ########
+
+## Write code that uses the tweepy library to search for tweets with a phrase of the user's choice (should use the Python input function), and prints out the Tweet text and the created_at value (note that this will be in GMT time) of the first THREE tweets with at least 1 blank line in between each of them, e.g.
+
+## TEXT: I'm an awesome Python programmer.
+## CREATED AT: Sat Feb 11 04:28:19 +0000 2017
+
+## TEXT: Go blue!
+## CREATED AT: Sun Feb 12 12::35:19 +0000 2017
+
+## .. plus one more.
+
+## You should cache all of the data from this exercise in a file, and submit the cache file along with your assignment. 
+
+## So, for example, if you submit your assignment files, and you have already searched for tweets about "rock climbing", when we run your code, the code should use CACHED data, and should not need to make any new request to the Twitter API. 
+## But if, for instance, you have never searched for "bicycles" before you submitted your final files, then if we enter "bicycles" when we run your code, it _should_ make a request to the Twitter API.
+
+## The lecture notes and exercises from this week will be very helpful for this. 
+## Because it is dependent on user input, there are no unit tests for this -- we will run your assignments in a batch to grade them!
+
+## We've provided some starter code below, like what is in the class tweepy examples.
+
+## **** For 50 points of extra credit, create another file called twitter_info.py that contains your consumer_key, consumer_secret, access_token, and access_token_secret, import that file here, and use the process we discuss in class to make that information secure! Do NOT add and commit that file to a public GitHub repository.
+
+## **** If you choose not to do that, we strongly advise using authentication information for an 'extra' Twitter account you make just for this class, and not your personal account, because it's not ideal to share your authentication information for a real account that you use frequently.
+
+## Get your secret values to authenticate to Twitter. You may replace each of these with variables rather than filling in the empty strings if you choose to do the secure way for 50 EC points
+consumer_key = twitter_info.consumer_key
+consumer_secret = twitter_info.consumer_secret
+access_token = twitter_info.access_token
+access_token_secret = twitter_info.access_token_secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser()) # Set up library to grab stuff from twitter with your authentication, and return it in a JSON-formatted way
+
+## Write the rest of your code here!
+
+def canocial_order(d):
+	alphabetized_keys = sorted(d.keys())
+	res = []
+	for k in alphabetized_keys:
+		res.append((k, d[k]))
+	return res 
+
+def requestURL(baseurl, params = {}):
+    req = requests.Request(method = 'GET', url = baseurl, params = canonical_order(params))
+    prepped = req.prepare()
+    return prepped.url
+
+# CACHING PATTERN 
+
+CACHE_FNAME = "cache.txt"
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+
+def search_tweet():
+	phrase = input("Enter a phrase to search on Twitter: ")
+	unique_identifier = "twitter_{}".format(phrase) # seestring formatting chapter
+	# see if that username+twitter is in the cache diction!
+	if unique_identifier in CACHE_DICTION: # if it is...
+		print('using cached data for', phrase)
+		twitter_results = CACHE_DICTION[unique_identifier] # grab the data from the cache!
+	else:
+		print('getting data from internet for', phrase)
+		twitter_results = api.search(q = phrase) # get it from the internet
+		# but also, save in the dictionary to cache it!
+		CACHE_DICTION[unique_identifier] = twitter_results # add it to the dictionary -- new key-val pair
+		# and then write the whole cache dictionary, now with new info added, to the file, so it'll be there even after your program closes!
+		f = open(CACHE_FNAME,'w') # open the cache file for writing
+		f.write(json.dumps(CACHE_DICTION)) # make the whole dictionary holding data and unique identifiers into a json-formatted string, and write that wholllle string to a file so you'll have it next time!
+		f.close()
+
+	tweet_phrases = twitter_results["statuses"]
+	return tweet_phrases
+
+tweepy_result = search_tweet()
+for tweet in tweepy_result[:3]:
+		print("TEXT: " + tweet["text"])
+		print("CREATED AT: " + tweet["created_at"])
+		print("\n")
+
+#### Recommended order of tasks: ####
+## 1. Set up the caching pattern start -- the dictionary and the try/except statement shown in class.
+## 2. Write a function to get twitter data that works with the caching pattern, so it either gets new data or caches data, depending upon what the input to search for is. You can model this off the class exercise from Tuesday.
+## 3. Invoke your function, save the return value in a variable, and explore the data you got back!
+## 4. With what you learn from the data -- e.g. how exactly to find the text of each tweet in the big nested structure -- write code to print out content from 3 tweets, as shown above.
+
+
+
+
+
+
+
+
